6e766844d253ebc03451f82b2cccc4fc5638b8cc
AdzikowskiSchoolWctc <Adzikowski@my.wctc.edu>
2025-11-11 19:22:49 -0600

    Commit 1.6: Post display working, going to finish showing all posts and then finish error handling

--- Program.cs
+++ Program.cs
@@ -65,6 +65,7 @@ while (!tester.Equals("q"))
     }
     var id = Console.ReadLine();
     var blogidchecker = 0;
+    
     foreach (var item in query)
     {
       if (Convert.ToInt32(id) == item.BlogId)
@@ -89,7 +90,7 @@ while (!tester.Equals("q"))
       Console.WriteLine("Enter post content: ");
       var content = Console.ReadLine();
 
-      var post = new Post { Title = name, Content = content, BlogId = Convert.ToInt32(id) };
+      var post = new Post { Title = name, Content = content, BlogId = Convert.ToInt32(id)};
       db.AddPost(post);
 
 
@@ -103,10 +104,45 @@ while (!tester.Equals("q"))
   else if (Convert.ToInt32(tester) == 4)
   {
     // display posts
+    Console.WriteLine("Please select the blog you wish to see, 0 for all posts: ");
+    var query = db.Blogs.OrderBy(b => b.BlogId);
+
+    foreach (var item in query)
+    {
+
+      Console.WriteLine($"{item.BlogId}) {item.Name}");
+    }
+    var id = Console.ReadLine();
+    if (Convert.ToInt32(id) < 0)
+    {
+
+    }
+    else if (Convert.ToInt32(id) == 0)
+    {
 
 
+
+    }
+    else if (Convert.ToInt32(id) != 0)
+    {
+        var posts = db.Posts.Where(b => b.BlogId == Convert.ToInt32(id));
+    
+    foreach( var item in posts)
+    {
+
+
+      
+            Console.WriteLine($"{item.Blog.Name}: {item.Title}, {item.Content}");
+        }
 
     
   }
+    }
+        else
+        {
+            
+        }
+
+    
 }
 logger.Info("Program ended");
