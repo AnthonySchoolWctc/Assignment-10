fe7a19fab9cce1504fa2b46262dc1d58acb45e2c
AdzikowskiSchoolWctc <Adzikowski@my.wctc.edu>
2025-11-11 18:59:39 -0600

    Commit 1.5: post adding should be working, will go back to ensure error handling

--- Blog.cs
+++ Blog.cs
@@ -4,4 +4,7 @@ public class Blog
   public string? Name { get; set; }
 
   public List<Post>? Posts { get; set; }
+  
+
+  
 }
--- Program.cs
+++ Program.cs
@@ -4,54 +4,55 @@ string path = Directory.GetCurrentDirectory() + "//nlog.config";
 
 // create instance of Logger
 var logger = LogManager.Setup().LoadConfigurationFromFile(path).GetCurrentClassLogger();
-
 logger.Info("Program started");
 var db = new DataContext();
 int count = 0;
 string tester = "";
 while (!tester.Equals("q"))
 {
-    Console.Write("Enter your selection: \n 1) Display all blogs \n 2) Add blog \n 3) Create post \n 4) Display posts ");
+  Console.Write("Enter your selection: \n 1) Display all blogs \n 2) Add blog \n 3) Create post \n 4) Display posts ");
   tester = Console.ReadLine();
   if (tester.Equals("q"))
-    {
+  {
     break;
-    }
+  }
   if (Convert.ToInt32(tester) == 1)
   {
     //display all blogs
-    var query = db.Blogs.OrderBy(b => b.Name);
+    var querybyname = db.Blogs.OrderBy(b => b.Name);
     count = 0;
 
-    foreach (var item in query)
+    foreach (var item in querybyname)
     {
       count++;
     }
     Console.WriteLine($"{count} blogs returned");
-  foreach (var item in query)
-  {
-    
-     Console.WriteLine(item.Name);
+    foreach (var item in querybyname)
+    {
+
+      Console.WriteLine(item.Name);
+    }
   }
-  } else if (Convert.ToInt32(tester) == 2)
+  else if (Convert.ToInt32(tester) == 2)
   {
-        //add blog
-        Console.Write("Enter a name for a new Blog: ");
-        var name = Console.ReadLine();
-        if (name.IsNullOrEmpty())
-        {
-          logger.Error("Blog Name cannot be null or empty");
-        }
-        else
-        {
-            var blog = new Blog { Name = name };
-            db.AddBlog(blog);
+    //add blog
+    Console.Write("Enter a name for a new Blog: ");
+    var name = Console.ReadLine();
+    if (name.IsNullOrEmpty())
+    {
+      logger.Error("Blog Name cannot be null or empty");
+    }
+    else
+    {
+      var blog = new Blog { Name = name };
+      db.AddBlog(blog);
 
-             logger.Info("Blog added - {name}", name);
-        }
-        
+      logger.Info("Blog added - {name}", name);
+    }
 
-    } else if (Convert.ToInt32(tester) == 3)
+
+  }
+  else if (Convert.ToInt32(tester) == 3)
   {
     // create posts
     Console.WriteLine("Please select the blog you wish to post to: ");
@@ -62,21 +63,50 @@ while (!tester.Equals("q"))
 
       Console.WriteLine($"{item.BlogId}) {item.Name}");
     }
-    tester = Console.ReadLine();
-        
-     foreach (var item in query)
-      {
-      if (Convert.ToInt32(tester) == item.BlogId)
-      {
-        
-      } 
-      }
-      
-
-    } else if (Convert.ToInt32(tester) == 4)
+    var id = Console.ReadLine();
+    var blogidchecker = 0;
+    foreach (var item in query)
     {
-        // display posts
+      if (Convert.ToInt32(id) == item.BlogId)
+      {
+        blogidchecker = 1;
+        break;
+      }
     }
-}
+    if (blogidchecker == 1)
+    {
 
+
+      Console.WriteLine("Enter a name for a new Post: ");
+      var name = Console.ReadLine();
+      if (name.IsNullOrEmpty())
+      {
+
+        logger.Error("Post Name cannot be null or empty");
+        break;
+      }
+
+      Console.WriteLine("Enter post content: ");
+      var content = Console.ReadLine();
+
+      var post = new Post { Title = name, Content = content, BlogId = Convert.ToInt32(id) };
+      db.AddPost(post);
+
+
+
+
+
+    }
+
+
+  }
+  else if (Convert.ToInt32(tester) == 4)
+  {
+    // display posts
+
+
+
+    
+  }
+}
 logger.Info("Program ended");
